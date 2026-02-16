08753f83a0c7deb45a66020dad91a37b2be9c19c
Eliran Takiya <elirantak@gmail.com>
2026-02-17 01:15:02 +0200

    adjust mongoUri for backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require("dotenv").config();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const conversationRoutes = require('./routes/conversations');
@@ -8,6 +9,17 @@ const app = express();
 app.use(express.json());
 app.use(cors({ methods: ['GET','POST','PUT','DELETE','OPTIONS'],}));
 app.use(bodyParser.json());
-mongoose.connect('mongodb://127.0.0.1:27017/commentTracker');
+const mongoUri = process.env.MONGO_URI;
+
+// mongoose.connect('mongodb://127.0.0.1:27017/commentTracker');
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
 app.use('/api/conversations', conversationRoutes);
-app.listen(5000, () => console.log('Backend running on http://localhost:5000'));
+// Listen on Render PORT or local 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
